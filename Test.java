b6fb33bc79f877e4bb38c55d203fd311507221ca
Adikumkar <103571396+Adikumkar@users.noreply.github.com>
2022-07-05 08:40:00 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,19 +1,28 @@
-package com.capgemini.springAssignment.Q1;
+package com.capgemini.springAssignment.Q2;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring1.xml");
 
-        // using the setter injection
-        Customer customer=(Customer) context.getBean("customer");
-        customer.customerDetail();
+        ApplicationContext context = new ClassPathXmlApplicationContext("springQ2.xml");
 
-        // using constructor injection
-        Customer customerCon=(Customer) context.getBean("customerCon");
-        customerCon.customerDetail();
+        // calling list of answer
+       Question question =(Question)context.getBean("Question");
+       question.displayAnswerList();
+
+
+       // calling set of answer
+        System.out.println("=====================================");
+        Question question1 =(Question)context.getBean("Question1");
+        question1.displayAnswerSet();
+
+        // calling map of answer
+        System.out.println("=====================================");
+        Question question2 =(Question)context.getBean("Question2");
+        question2.displayAnswerMap();
 
     }
 }
